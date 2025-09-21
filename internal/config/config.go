f2ec5cd6da08767bbe00e2c737fb4ae8e3efe829
Alexandr Kobzev <sasha_kobzev18@mail.ru>
2025-09-21 15:47:57 +0300

    feature: config and logger setup

---
+++ internal/config/config.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env       string          `yaml:"env" env-required:"true"`
+	TokenTTL  time.Duration   `yaml:"token_ttl" env-required:"true"`
+	Postrgres PostrgresConfig `yaml:"postgres"`
+	GRPC      GRPCConfig      `yaml:"grpc"`
+}
+
+type PostrgresConfig struct {
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+	Name     string `yaml:"name"`
+	Port     int    `yaml:"port"`
+}
+
+type GRPCConfig struct {
+	Port    int           `yaml:"port"`
+	Timeout time.Duration `yaml:"timeout"`
+}
+
+func Load() *Config {
+	path := os.Getenv("CONFIG_PATH")
+	if path == "" {
+		panic("CONFIG_PARH is not set")
+	}
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		panic("config file does not exist: " + path)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(path, &cfg); err != nil {
+		panic("failed to read config: " + err.Error())
+	}
+	return &cfg
+}
