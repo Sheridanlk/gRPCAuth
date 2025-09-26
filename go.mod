f887bd03fa7da2c9ce6dfe99e92cb82298caf64a
Alexandr Kobzev <sasha_kobzev18@mail.ru>
2025-09-26 21:02:14 +0300

    feature: database path

--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/golang-jwt/jwt/v5 v5.3.0 // indirect
 	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.10.9
 	golang.org/x/crypto v0.42.0 // indirect
 	golang.org/x/net v0.43.0 // indirect
 	golang.org/x/sys v0.36.0 // indirect
---
+++ internal/storage/posgres/postgres.go
@@ -0,0 +1,122 @@
+package posgres
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	"sso/internal/domain/models"
+	"sso/internal/storage"
+
+	"github.com/lib/pq"
+)
+
+type Storage struct {
+	db *sql.DB
+}
+
+func New(user string, password string, dbname string, port int) (*Storage, error) {
+	const op = "storage.postgresql.New"
+	connStr := fmt.Sprintf("user=%s password=%s dbname=%s port=%v sslmode=disable", user, password, dbname, port)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, fmt.Errorf("%s: can't connect to database: %w", op, err)
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("%s: can't ping to database: %w", op, err)
+	}
+
+	return &Storage{db: db}, nil
+}
+
+// SaveUser saves user to db
+func (s *Storage) SaveUser(ctx context.Context, email string, passHash []byte) (int64, error) {
+	const op = "storage.postgres.SaveUser"
+
+	stmt, err := s.db.Prepare("INSERT INTO users(email, pass_hash) VAULUES($1, $2)")
+	if err != nil {
+		return 0, fmt.Errorf("%s: can't prepare query: %w", op, err)
+	}
+
+	var id int64
+	err = stmt.QueryRowContext(ctx, email, passHash).Scan(&id)
+	if err != nil {
+		if postgresErr, ok := err.(*pq.Error); ok && postgresErr.Code == "23505" {
+			return 0, fmt.Errorf("%s: %w", op, storage.ErrUserExists)
+		}
+
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return id, nil
+}
+
+// User returns user by email
+func (s *Storage) User(ctx context.Context, email string) (models.User, error) {
+	const op = "storage.postgres.User"
+
+	stmt, err := s.db.Prepare("SECECT id, email, pass_hash FROM users WHERE email = $1")
+	if err != nil {
+		return models.User{}, fmt.Errorf("%s: can't prepare query: %w", op, err)
+	}
+
+	var user models.User
+
+	err = stmt.QueryRowContext(ctx, email).Scan(&user.ID, &user.Email, &user.PassHash)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return models.User{}, fmt.Errorf("%s: %w", op, storage.ErrUserNotFound)
+		}
+		return models.User{}, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return user, nil
+}
+
+// IsAdmin returns true if user is an administrator
+func (s *Storage) IsAdmin(ctx context.Context, userID int64) (bool, error) {
+	const op = "storage.postgres.IsAdmin"
+
+	stmt, err := s.db.Prepare("SELECT isAmin FORM users WHERE user_id = $1")
+	if err != nil {
+		return false, fmt.Errorf("%s: can't prepare query: %w", op, err)
+	}
+
+	var isAdmin bool
+
+	err = stmt.QueryRowContext(ctx, userID).Scan(&isAdmin)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return false, fmt.Errorf("%s: %w", op, storage.ErrUserNotFound)
+		}
+
+		return false, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return isAdmin, nil
+}
+
+// App returns app by app id
+func (s *Storage) App(ctx context.Context, appID int) (models.App, error) {
+	const op = "storage.postgres.App"
+
+	stmt, err := s.db.Prepare("SELECT id, name, secret FROM apps WHERE id = $1")
+	if err != nil {
+		return models.App{}, fmt.Errorf("%s: can't prepare query: %w", op, err)
+	}
+
+	var app models.App
+
+	err = stmt.QueryRowContext(ctx, appID).Scan(&app.ID, &app.Name, &app.Secret)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return models.App{}, fmt.Errorf("%s: %w", op, storage.ErrAppNotFound)
+		}
+
+		return models.App{}, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return app, nil
+}
